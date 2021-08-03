98660bd265ec7627e61c36ab9cb171970bef29b6
Russell Gordon <rgordon@lcs.on.ca>
2021-08-03 13:57:23 -0400

    Initial Commit

---
+++ SimpleActorChallengeSolution/SimpleActorChallengeSolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleActorChallengeSolutionApp.swift
+//  SimpleActorChallengeSolution
+//
+//  Created by Russell Gordon on 2021-08-03.
+//
+
+import SwiftUI
+
+@main
+struct SimpleActorChallengeSolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
